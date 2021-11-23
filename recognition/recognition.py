8d40fad9d19eef4c1bd1a240faedfdaee8567b16
michkied <michkied@users.noreply.github.com>
2021-11-23 18:06:33 +0100

    Improve caption handling and text wrapping

--- recognition/recognition.py
+++ recognition/recognition.py
@@ -6,21 +6,27 @@ import threading
 r = sr.Recognizer()
 r.pause_threshold = 0.5
 
-# print(sr.Microphone.list_microphone_names())
-
 
 def clear_output():
     with open('recognition/output.txt', 'w', encoding='UTF-8') as f:
         f.write('Słucham... (wciśnij ESC by zamknąć)')
 
 
-def wrapping(text_to_wrap):
+def get_wrapped_lines(text_to_wrap):
     # Get display data
     with open('gui/display_data.json', 'r', encoding='UTF-8') as f:
         data = json.loads(f.read())
     num_of_chars = int(data['width'] / (data['fontsize'] * 0.75))
 
     lines = []
+
+    # Handle lines carried from last time
+    if text_to_wrap.count('\n') > 0:
+        ttw_lines = text_to_wrap.split('\n')
+        for line in ttw_lines[:-1]:
+            lines.append(line)
+        text_to_wrap = ttw_lines[-1]
+
     while len(text_to_wrap) > num_of_chars:
         chunk = text_to_wrap[:num_of_chars][::-1]  # Limit the chunk to max number of chars and flip it
         line = chunk[chunk.find(' ') + 1:][::-1]  # Find the end of the line (first space of the flipped chunk), crop the string, flip it back
@@ -33,7 +39,7 @@ def wrapping(text_to_wrap):
 
 class Recognition:
     def __init__(self):
-        self.previous_line = ''
+        self.carry = ''
         self.chunk_queue = []
         self.next_chunk_id = 0
 
@@ -46,19 +52,17 @@ class Recognition:
 
             if audio is not None:
                 self.chunk_queue.append(chunk_id)
-                recognition_thread = threading.Thread(target=self.analyse_and_save, args=(audio, chunk_id))
+                recognition_thread = threading.Thread(target=self.process_audio, args=(audio, chunk_id))
                 recognition_thread.start()
 
-    def analyse_and_save(self, audio, chunk_id):
+    def process_audio(self, audio, chunk_id):
 
         # Perform speech recognition
         try:
             new_line = r.recognize_google(audio, language='pl-PL')
-
         except sr.UnknownValueError:
             clear_output()
-            self.previous_line = ''
-
+            self.carry = ''
         except sr.RequestError as e:
             with open('recognition/output.txt', 'w', encoding='UTF-8') as f:
                 f.write('Wystąpił błąd.')
@@ -66,23 +70,38 @@ class Recognition:
         else:
             while chunk_id != min(self.chunk_queue):  # Ensure chunks are in the right order
                 time.sleep(0.1)
+            self.save(new_line)
 
-            lines = wrapping(self.previous_line + new_line)
-            new_text = '\n'.join(lines[:2])
-            self.previous_line = lines[-1] + ' '
-            with open('recognition/output.txt', 'w', encoding='UTF-8') as f:
-                f.write(new_text)
+        self.chunk_queue.remove(chunk_id)
 
-            # Give time to read
-            time.sleep(len(new_line)/28)
-            print('done sleeping')
+    def save(self, new_line):
+        lines = get_wrapped_lines(self.carry + new_line)
+        new_text = '\n'.join(lines[:2])
 
-            self.chunk_queue.remove(chunk_id)
+        if len(lines) == 1:
+            self.carry = lines[-1] + '\n'  # If there is only one line, keep it
+        else:
+            if len(lines) == 2:  # If there are two lines, don't keep them
+                self.carry = ''
+            else:
+                self.carry = '\n'.join(lines[2:]) + ' '  # If there are more than 2 lines, display them next time
+
+        with open('recognition/output.txt', 'w', encoding='UTF-8') as f:
+            f.write(new_text)
+
+        # Give time to read
+        time.sleep(len(new_line) / 25)
 
     def listen(self):
         try:
             with sr.Microphone(device_index=1) as source:
                 return r.listen(source, phrase_time_limit=10, timeout=12)
+
         except sr.WaitTimeoutError:
+
+            # Wait until all is read
+            while self.chunk_queue:
+                time.sleep(0.5)
             clear_output()
+
             return None
